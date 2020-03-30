6207ca5771eee497496884ceb214629fb7145e6f
norio-tech <fukagawa0420@gmail.com>
2020-03-30 16:05:15 +0900

    グループユーザーのインクリメンタルサーチ機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,7 @@ class GroupsController < ApplicationController
   end
 
   def update
+    binding.pry
     @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to group_messages_path(@group.id), notice: 'グループを変更しました'
